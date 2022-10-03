c61ff70e9c4b32cc29ba07e863a69df40ed671cd
mucollabo <mucollabo@gmail.com>
2022-10-03 18:28:45 +0900

    complete test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,20 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es6": true
+        "es6": true,
+        "jest": true,
     },
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "__dirname": true,
+        "process": true
     },
     "parserOptions": {
         "ecmaVersion": 2018
     },
     "rules": {
+        "no-console": "off"
     }
 };
