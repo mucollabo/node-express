b4e365f203a795222d017b66f990ec489712a251
mucollabo <mucollabo@gmail.com>
2022-10-19 22:20:29 +0900

    complete chap7

--- meadowlark.js
+++ meadowlark.js
@@ -2,21 +2,34 @@ const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const app = express()
+const weatherMiddleware = require('./lib/middleware/weather')
 
 // set the handlebars view engine
 app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        },
+    },
 }))
+
 app.set('view engine', 'handlebars')
 
 // add static middle ware
 app.use(express.static(__dirname + '/public'))
+
+app.use(weatherMiddleware)
 
 const port = process.env.PORT || 3000
 
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
+
+app.get('/section-test', handlers.sectionTest)
 
 // custom 404 page
 app.use(handlers.notFound)
