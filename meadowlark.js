cf2497b0af1ca4f4d7ada3f5ccfae50da0535bf4
mucollabo <mucollabo@gmail.com>
2022-11-03 00:34:28 +0900

    complete chap8

--- meadowlark.js
+++ meadowlark.js
@@ -3,8 +3,10 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const app = express()
 const weatherMiddleware = require('./lib/middleware/weather')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 
-// set the handlebars view engine
+// configure handlebars view engine
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
     helpers: {
@@ -23,6 +25,9 @@ app.use(express.static(__dirname + '/public'))
 
 app.use(weatherMiddleware)
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const port = process.env.PORT || 3000
 
 app.get('/', handlers.home)
@@ -30,6 +35,23 @@ app.get('/', handlers.home)
 app.get('/about', handlers.about)
 
 app.get('/section-test', handlers.sectionTest)
+
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+// vacation photo contest
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if(err) return res.status(500).send({ error: err.message })
+        handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
+})
 
 // custom 404 page
 app.use(handlers.notFound)
